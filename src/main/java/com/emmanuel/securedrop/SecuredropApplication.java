e4f1069dcf05c72d471d0ab6e2e6e7c8e0933723
Emmanuel Opara <nnamdi93@gmail.com>
2026-03-05 14:36:09 -0600

    Initial Spring Boot skeleton(Java 21, web, security, JPA)

---
+++ src/main/java/com/emmanuel/securedrop/SecuredropApplication.java
@@ -0,0 +1,13 @@
+package com.emmanuel.securedrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emmanuel/securedrop/SecuredropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emmanuel.securedrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
